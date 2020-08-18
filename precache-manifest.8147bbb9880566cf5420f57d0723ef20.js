2cc709dfaa4cc52aa2df7ffbfd12632615d9d121
truong giang <giangdt.bhsoft@gmail.com>
2020-08-18 13:13:14 +0700

    Updates

---
+++ precache-manifest.8147bbb9880566cf5420f57d0723ef20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f93cdbca8cf2ac33f70aa93abac428",
+    "url": "/schedule/index.html"
+  },
+  {
+    "revision": "fd2583fce86b866845df",
+    "url": "/schedule/static/css/main.24713b4a.chunk.css"
+  },
+  {
+    "revision": "9d683e7d1b7c49dd4782",
+    "url": "/schedule/static/js/2.3170a700.chunk.js"
+  },
+  {
+    "revision": "381d780c75076efaf60a7392ae15e75c",
+    "url": "/schedule/static/js/2.3170a700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd2583fce86b866845df",
+    "url": "/schedule/static/js/main.08e272ec.chunk.js"
+  },
+  {
+    "revision": "fc6a2e1e53919748037a",
+    "url": "/schedule/static/js/runtime-main.9e2cda7b.js"
+  },
+  {
+    "revision": "17b08a8e00500b2b1a3cd038c6e53791",
+    "url": "/schedule/static/media/avatar-default.17b08a8e.png"
+  },
+  {
+    "revision": "e24c884b00450a17465dbc2a6c90149e",
+    "url": "/schedule/static/media/background.e24c884b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/schedule/precache-manifest.1b62d7fb5bc36527912456eed6a2720a.js"
+  "/schedule/precache-manifest.8147bbb9880566cf5420f57d0723ef20.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/schedule/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

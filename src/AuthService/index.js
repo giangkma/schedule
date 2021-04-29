1b35bfa1874bcc93de8d2161c0abc625f3813467
Dang Truong Giang <giangdt.bhsoft@gmail.com>
2021-04-29 20:25:04 +0700

    update api

--- src/AuthService/index.js
+++ src/AuthService/index.js
@@ -1,18 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default class AuthService {
-    constructor(domain) {
-        this.domain =
-            domain || 'http://167.179.115.162:5000/api/schedule/guest';
-        this.proxy = "https://cors-anywhere.herokuapp.com/";
-    }
+  constructor(domain) {
+    this.domain = domain || "https://tkbkma.herokuapp.com/api/schedule/guest";
+  }
 
-    login = (studentAccount, studentPassword) => {
-        const urlencoded = new URLSearchParams();
-        urlencoded.append('studentAccount', studentAccount);
-        urlencoded.append('studentPassword', studentPassword);
-        return axios.post(`${this.proxy + this.domain}`, urlencoded).then((res) => {
-            return Promise.resolve(res);
-        });
-    };
+  login = (studentAccount, studentPassword) => {
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("studentAccount", studentAccount);
+    urlencoded.append("studentPassword", studentPassword);
+    return axios.post(`${this.domain}`, urlencoded).then((res) => {
+      return Promise.resolve(res);
+    });
+  };
 }
